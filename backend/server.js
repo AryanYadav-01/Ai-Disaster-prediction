10f7741ab13bfd26212a54990e9fa10d0504fab7
Aryan Yadav <ay630147gmail.com@>
2026-03-25 20:25:27 +0530

    fixed port for render

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,8 @@ app.post("/flood-risk", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Backend running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Backend running on port ${PORT}`);
 });
